e50d9345ccfa13ab4234923e8c71f4799184f5a7
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-07-03 15:03:57 -0700

    Update file-manipulation.py

--- file-manipulation.py
+++ file-manipulation.py
@@ -3,15 +3,15 @@
 import os
 
 myf = open('1.txt', 'w')
-print("wudup\n", file=myf)
+print('wudup', file=myf)
 myf.close()
-print("created 1.txt")
+print('created 1.txt')
 input('--> ')
 
 os.rename('1.txt', '2.txt')   #REM 2.txt gets replaced even if 2.txt exists
-print("moved 1.txt to 2.txt\n")
+print('moved 1.txt to 2.txt')
 input('--> ')
 
 os.remove('2.txt')
-print("removed 2.txt\n")
-print("exiting..\n")
+print('removed 2.txt')
+print('exiting..')
