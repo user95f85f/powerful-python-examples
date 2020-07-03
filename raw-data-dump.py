93a395431de085b765fe522d8d62e2b0d9f5ca2b
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-07-02 19:02:04 -0700

    Update raw-data-dump.py

--- raw-data-dump.py
+++ raw-data-dump.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-def mydump(_a):
-   arOut = []
-   len_a = len(_a)
-   i = 0
-   while i < len_a:
-      arOut.append('{:06d}  '.format(i))
-      for j in range(16):
-         if (i + j) < len_a:
-            myByte = _a[i + j];
-            arOut.append('{:02X} '.format(myByte))
-      i += 16
-   return ''.join(arOut)
 
-print(mydump(b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a'));
+myData = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a'
+arOut = []
+len_myData = len(myData)
+i = 0
+while i < len_myData:
+   arOut.append('{:06d}  '.format(i))
+   for j in range(16):
+      if (i + j) < len_myData:
+         myByte = myData[i + j];
+         arOut.append('{:02X} '.format(myByte))
+   i += 16
+
 #000000  00 01 02 03 04 05 06 07 08 09 0A
+print(''.join(arOut))
