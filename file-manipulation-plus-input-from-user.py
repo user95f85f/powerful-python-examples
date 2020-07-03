85dc14f3234e6262b322ab014338b10583138720
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-07-03 10:16:38 -0700

    Create file-manipulation-plus-input-from-user.py

---
+++ file-manipulation-plus-input-from-user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import os #for rename of file and removal of file functionality
+
+print("give me input to put into a.txt\n");
+with open('a.txt', 'w') as myf:
+   while True:
+      szInput = input('--> ')
+      if szInput == '':
+         break
+      print(szInput, file=myf)
+
+print(str(myf.closed))  #True
+print("put contents into a.txt\n")
+input('--> ')
+
+os.rename('a.txt', 'b.txt')
+print("moved a.txt to b.txt\n")
+input('--> ')
+
+os.remove('b.txt')
+print("removed b.txt\n")
+input('--> ')
+print("exiting..\n")
