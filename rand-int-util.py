4a7f2e3321a8c6e5af08fe76c7240f0c58766b21
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-07-02 19:11:14 -0700

    Create rand-int-util.py

---
+++ rand-int-util.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import random
+import sys
+
+if len(sys.argv) != 3:
+   exit(1)
+
+print(str(random.randint(int(sys.argv[1]), int(sys.argv[2]))))
+
+
+#~$ python3 randint.py 1 2
+#2
+#~$ python3 randint.py 1 2
+#1
+#~$ python3 randint.py 1 2
+#2
+#~$ python3 randint.py 1 2
+#2
+#~$ python3 randint.py 1 6
+#5
+#~$ python3 randint.py 1 6
+#1
+#~$ python3 randint.py 1 6
+#2
+#~$ python3 randint.py 1 6
+#3
+#~$ python3 randint.py 1 6
+#2
+#~$ python3 randint.py 1 6
+#1
