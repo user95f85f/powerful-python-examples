86264eb00837622d287def79b27662a60dd6fd13
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-07-03 23:46:00 -0700

    Update string-multilined.py

--- string-multilined.py
+++ string-multilined.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 szText = """dear {name},
 I think {verb1}
 therefore {verb2}
@@ -7,8 +8,8 @@ yada yada yada sofoh eekwahna dooshor
 adi, {mr}
 -{author}"""
 
-print(szText.format(name='Joe', verb1='you are awesome', verb2='everything is grand', mr='senior', author='yours truly, Ayebei'))
-#dear Joe,
+print(szText.format(name=os.getlogin(), verb1='you are awesome', verb2='everything is grand', mr='senior', author='yours truly, Ayebei'))
+#dear debian-user,
 #I think you are awesome
 #therefore everything is grand
 #yada yada yada sofoh eekwahna dooshor
