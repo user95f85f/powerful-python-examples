0524da6c596f9ce1cd82667ad701d1f941956c16
user95f85f <48297795+user95f85f@users.noreply.github.com>
2020-07-03 23:49:19 -0700

    Update string-multilined.py

--- string-multilined.py
+++ string-multilined.py
@@ -4,14 +4,14 @@ import os
 szText = """dear {name},
 I think {verb1}
 therefore {verb2}
-yada yada yada sofoh eekwahna dooshor
+we are stuck in {place}
 adi, {mr}
 -{author}"""
 
-print(szText.format(name=os.getlogin(), verb1='you are awesome', verb2='everything is grand', mr='senior', author='yours truly, Ayebei'))
+print(szText.format(name=os.getlogin(), verb1='you are awesome', verb2='everything is grand', place=os.getcwd(), mr='senior', author='yours truly, Ayebei'))
 #dear debian-user,
 #I think you are awesome
 #therefore everything is grand
-#yada yada yada sofoh eekwahna dooshor
+#we are stuck in /home/debian-user/power-python
 #adi, senior
 #-yours truly, Ayebei
